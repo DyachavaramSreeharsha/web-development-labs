d7f8556cfe61a80e5862bf488ae66271f1cc690f
DyachavaramSreeharsha <sreeharshadyachavaram@gmail.com>
2026-04-30 13:49:03 -0400

    message

---
+++ .theia/scope/scope_lab.js
@@ -0,0 +1,11 @@
+// Global scope
+var globalVar ="I'm global variable";
+let globalLet = "I'm a global constant";
+
+{
+    // Block scope
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped Let";
+    const blockConst = "I'm a block-scoped const";
+}
+
